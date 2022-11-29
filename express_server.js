30b889d8be395c24d39340b04f07935352244db8
RickCarr <rick.carr@telus.com>
2022-11-29 14:48:06 -0800

    added urls_show and app.get to render via express_server

--- express_server.js
+++ express_server.js
@@ -8,10 +8,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//url index
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+//url shortener index
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
