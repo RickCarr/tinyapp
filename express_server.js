62fe4c0fdbb8eb7c257d686509881c4839a67342
RickCarr <rick.carr@telus.com>
2022-11-29 15:32:43 -0800

    added urls_new and updated express server to handle response

--- express_server.js
+++ express_server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+function generateRandomString() {
+  let randomChars = "";
+  const alphaNum = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i <= 5; i++) {
+    //62 characters in the alphanumeric possibilities including capitalized letters
+    randomChars += alphaNum.charAt(Math.floor(Math.random() * 62));
+  }
+  return randomChars;
+}
 
 app.set("view engine", "ejs");
 
@@ -8,12 +17,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//express encodes urls being input
+app.use(express.urlencoded({ extended: true }));
+
 //url index
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 //url shortener index
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -31,7 +48,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//create a new url submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-  
+
 });
